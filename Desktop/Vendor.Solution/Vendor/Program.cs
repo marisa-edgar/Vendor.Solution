d716203ac3db8dcd22a650058ed085f0a2eae5ad
marisa-edgar <marisaedgar1212@gmail.com>
2022-03-04 09:33:58 -0800

    add boiler plate and rename files

---
+++ Desktop/Vendor.Solution/Vendor/Program.cs
@@ -0,0 +1,6 @@
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+
+namespace Vendor
+{
+  public class Program
---
+++ Desktop/Vendor.Solution/Vendor/Startup.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Vendor
+{
+  public class Startup
