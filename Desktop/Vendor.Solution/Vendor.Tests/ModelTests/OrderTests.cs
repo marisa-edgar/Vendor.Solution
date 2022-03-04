8df08813f937d1cdb1afeac7f28a301c7e54514a
marisa-edgar <marisaedgar1212@gmail.com>
2022-03-04 12:56:50 -0800

    write fifth test

--- Desktop/Vendor.Solution/Vendor.Tests/ModelTests/OrderTests.cs
+++ Desktop/Vendor.Solution/Vendor.Tests/ModelTests/OrderTests.cs
@@ -43,11 +43,25 @@ namespace Vendor.Tests
     [TestMethod]
     public void GetAll_ReturnsEmptyList_OrderList()
     {
+      string description01 = "new order";
+      string description02 = "order 2";
+      Order newOrder1 = new Order(description01);
+      Order newOrder2 = new Order(description02);
       List<Order> newList = new List<Order> { };
       List<Order> result = Order.GetAll();
       CollectionAssert.AreEqual(newList, result);
-}
+    }
 
-
+    [TestMethod]
+    public void GetAll_ReturnsItems_OrderList()
+    {
+      string description01 = "new order";
+      string description02 = "order 2";
+      Order newOrder1 = new Order(description01);
+      Order newOrder2 = new Order(description02);
+      List<Order> newList = new List<Order> { newOrder1, newOrder2 };
+      List<Order> result = Order.GetAll();
+      CollectionAssert.AreEqual(newList, result);
+    }
   }
 }
