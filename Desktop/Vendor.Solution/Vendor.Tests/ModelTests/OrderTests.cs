5185b32f63929cb43d4f4dcd7fe499ad92a9ec0b
marisa-edgar <marisaedgar1212@gmail.com>
2022-03-04 10:02:27 -0800

    write first test

---
+++ Desktop/Vendor.Solution/Vendor.Tests/ModelTests/OrderTests.cs
@@ -0,0 +1,22 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using Vendor.Models;
+using System;
+
+namespace Vendor.Tests
+{
+  [TestClass]
+  public class OrderTests : IDisposable
+  {
+
+    public void Dispose()
+    {
+      Order.ClearAll();
+    }
+
+    [TestMethod]
+    public void OrderConstructor_CreatesInstanceOfOrder_Order()
+    {
+      Order newOrder = new Order("test");
+      Assert.AreEqual(typeof(Order), newOrder.GetType());
+    }
