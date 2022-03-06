c54f83f556e817425437fbc8b47eb9f018d05483
marisa-edgar <marisaedgar1212@gmail.com>
2022-03-06 00:07:04 -0800

    debugging views

--- Desktop/Vendor.Solution/VendorOrder/Controllers/VendorsController.cs
+++ Desktop/Vendor.Solution/VendorOrder/Controllers/VendorsController.cs
@@ -1,55 +1,54 @@
-// using Microsoft.AspNetCore.Mvc;
-// using VendorOrder.Models;
-// using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using VendorOrder.Models;
+using System.Collections.Generic;
 
-// namespace VendorOrder.Controllers
-// {
-//   public class OrderController : Controller
-//   {
-//   [HttpGet("/vendors")]
-//     public ActionResult Index()
-//     {
-//       List<Vendor> allVendor = Vendor.GetAll();
-//       return View(allVendor);
-//     }
+namespace VendorOrder.Controllers
+{
+  public class VendorsController : Controller
+  {
+  [HttpGet("/vendors")]
+    public ActionResult Index()
+    {
+      List<Vendor> allVendor = Vendor.GetAll();
+      return View(allVendor);
+    }
 
-  //   [HttpGet("/vendors/new")]
-  //   public ActionResult New()
-  //   {
-  //     return View();
-  //   }
+    [HttpGet("/vendors/new")]
+    public ActionResult New()
+    {
+      return View();
+    }
 
-  //   [HttpPost("/vendors")]
-  //   public ActionResult Create(string vendorName)
-  //   {
-  //     Vendor newVendor = new Vendor(vendorName);
-  //     return RedirectToAction("Index");
-  //   }
+    [HttpPost("/vendors")]
+    public ActionResult Create(string vendorName)
+    {
+      Vendor newVendor = new Vendor(vendorName);
+      return RedirectToAction("Index");
+    }
 
-  //   [HttpGet("/vendors/{id}")]
-  //   public ActionResult Show(int id)
-  //   {
-  //     Dictionary<string, object> model = new Dictionary<string, object>();
-  //     Vendor selectedVendor = Vendor.Find(id);
-  //     List<Order> vendorItems = selectedVendor.Orders;
-  //     model.Add("vendor", selectedVendor);
-  //     model.Add("orders", vendorOrders);
-  //     return View(model);
-  //   }
+    [HttpGet("/vendors/{id}")]
+    public ActionResult Show(int id)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Vendor selectedVendor = Vendor.Find(id);
+      List<Order> vendorOrders = selectedVendor.Orders;
+      model.Add("vendor", selectedVendor);
+      model.Add("orders", vendorOrders);
+      return View(model);
+    }
 
-  //   [HttpPost("/vendors/{vendorId}/orders")]
-  //   public ActionResult Create(int vendorId, string orderDescription)
-  //   {
-  //     Dictionary<string, object> model = new Dictionary<string, object>();
-  //     Vendor foundVendor = Vendor.Find(VendorId);
-  //     Order newOrder = new Order(orderDescription);
-  //     foundVendor.AddOrder(newOrder);
-  //     List<Order> vendorOrders = foundVendor.Orders;
-  //     model.Add("orders", vendorOrders);
-  //     model.Add("vendor", foundVendor);
-  //     return View("Show", model);
-  //   }
+    [HttpPost("/vendors/{vendorId}/orders")]
+    public ActionResult Create(int vendorId, string orderDescription)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Vendor foundVendor = Vendor.Find(vendorId);
+      Order newOrder = new Order(orderDescription);
+      foundVendor.AddOrder(newOrder);
+      List<Order> vendorOrders = foundVendor.Orders;
+      model.Add("orders", vendorOrders);
+      model.Add("vendor", foundVendor);
+      return View("Show", model);
+    }
 
-  // }
-//   }
-// }
+  }
+}
