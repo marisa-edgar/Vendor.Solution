24b384e359b81b8f4006900f0fab70358c136336
marisa-edgar <marisaedgar1212@gmail.com>
2022-03-05 23:00:40 -0800

    pass test 7

--- Desktop/Vendor.Solution/VendorOrder.Tests/ModelTests/VendorTests.cs
+++ Desktop/Vendor.Solution/VendorOrder.Tests/ModelTests/VendorTests.cs
@@ -14,5 +14,14 @@ namespace VendorOrder.Tests
       Vendor newVendor = new Vendor("test vendor");
       Assert.AreEqual(typeof(Vendor), newVendor.GetType());
     }
+
+    [TestMethod]
+    public void GetName_ReturnsName_String()
+    {
+      string name = "test vendor";
+      Vendor newVendor = new Vendor(name);
+      string result = newVendor.Name;
+      Assert.AreEqual(name, result);
+    }
   }
 }
