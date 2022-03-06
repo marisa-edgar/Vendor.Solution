053565e7a0a99743c2ffbe36483f03d012f2b422
marisa-edgar <marisaedgar1212@gmail.com>
2022-03-05 23:16:34 -0800

    write test 10

--- Desktop/Vendor.Solution/VendorOrder.Tests/ModelTests/VendorTests.cs
+++ Desktop/Vendor.Solution/VendorOrder.Tests/ModelTests/VendorTests.cs
@@ -50,5 +50,16 @@ namespace VendorOrder.Tests
       List<Vendor> result = Vendor.GetAll();
       CollectionAssert.AreEqual(newList, result);
     }
+
+    [TestMethod]
+    public void Find_ReturnsCorrectVendor_Vendor()
+    {
+      string name01 = "vendor1";
+      string name02 = "vendor2";
+      Vendor newVendor1 = new Vendor(name01);
+      Vendor newVendor2 = new Vendor(name02);
+      Vendor result = Vendor.Find(2);
+      Assert.AreEqual(newVendor2, result);
+    }
   }
 }
